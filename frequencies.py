eac453170bee36e07dbf60bf5ba9008142b18613
Michael Seiranian <michael.seiranian@kcl.ac.uk>
2022-10-02 21:36:36 +0100

    Add frequency functionality

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if frequencies.get(str(item)) == None:
+            frequencies[str(item)]=1
+        else:
+            frequencies[str(item)]+=1
     return frequencies
